2a896e8539ce0d8b218ee3c3c36e451d6b422396
divacs <sonja.divac@yahoo.com>
2025-08-16 18:52:11 +0200

    connection with db; added first migration

---
+++ BookNestProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookNestProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ BookNestProject/Migrations/20250816165110_addCategoryTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookNestProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
